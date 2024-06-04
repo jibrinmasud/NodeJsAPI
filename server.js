e1600fa9c8538e847b6aa583a2c4980b3cb20089
jibrinmasud <jibrinmasud494@gmail.com>
2024-06-04 12:50:03 +0100

    added the update endpoint

--- server.js
+++ server.js
@@ -20,11 +20,11 @@ var ingredients = [
     }
 ];
 
-app.get('/', function(req, res){
+app.get('/ingredients', function(req, res){
     res.send(ingredients);
 });
 
-app.post('/', function(req, res){
+app.post('/ingredients', function(req, res){
     var ingredient = req.body;
     if(!ingredient || ingredient.text === ""){
         res.status(500).send({error:"Your ingredients most have a text"});
@@ -34,6 +34,41 @@ app.post('/', function(req, res){
     }
 });
 
+app.put('/ingredients/:ingredientId', function(req, res){
+    var ingredientId = req.params.ingredientId;
+    var newtext = req.body.text;
+
+    if(!newtext || newtext ==="")
+        {
+            res.status(500).send({error:"text most be provided"})
+        }
+    for(var x = 0; x < ingredients.length; x++)
+        {
+            var ing = ingredients[x];
+            if(ing.id === req.params.ingredientId)
+                {
+                    ingredients[x].text = newtext;
+                    break;
+                }
+        }
+
+        res.send(ingredients);
+       
+});
+
 app.listen(3000, function(){
     console.log('First API Rnning on port 3000!')
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
